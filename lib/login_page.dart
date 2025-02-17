794e771489721ba651d2042502e00206e1bbdefd
Darwynkjoy <darwynkjoy@gmail.com>
2025-02-17 13:24:17 +0530

    daily push

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,29 +10,60 @@ class _loginPageState extends State<LoginPage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login",style: TextStyle(fontSize: 30,color: Colors.blue),),
-      ),
+      backgroundColor: Colors.black,
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(height: 100,),
+            Text("Login",style: TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold),),
+            Text("Welcome back",style: TextStyle(fontSize: 20,color: Colors.white),),
+            SizedBox(height: 50,),
             TextField(
+              cursorColor: Colors.blue,
               controller: emailController,
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(width: 2,color: Colors.blue),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(width: 2,color: Colors.white),
+                ),
                   label: Text("Email"),labelStyle: TextStyle(color: Colors.blue),
                   hintText: "Example@email.com",hintStyle: TextStyle(fontSize: 15,color: Colors.grey)),
             ),
+            SizedBox(height: 20,),
             TextField(
+              cursorColor: Colors.blue,
               controller: passwordController,
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(width: 2,color: Colors.blue),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(width: 2,color: Colors.white),
+                ),
                   label: Text("Password"),labelStyle: TextStyle(color: Colors.blue),
-                  hintText: "enter your password",hintStyle: TextStyle(fontSize: 15,color: Colors.grey)),
-            )
+                  hintText: "Enter your password",hintStyle: TextStyle(fontSize: 15,color: Colors.grey)),
+            ),
+            Row(
+              children: [
+                Spacer(),
+                TextButton(onPressed: (){}, child: Text("Forgot Password ?",style: TextStyle(fontSize: 16,color: Colors.blue,fontWeight: FontWeight.bold),)),
+              ],
+            ),
+            Center(
+              child: SizedBox(
+                height: 45,
+                width: 300,
+                child: ElevatedButton(onPressed: (){}, child: Text("Login",style: TextStyle(fontSize: 16,color: Colors.blue,fontWeight: FontWeight.bold),))),
+            ),
+            Divider(color: Colors.white,),
           ],
         ),
       ),
